b8accfe405bbdd36a87bda3fb7dcdf25b7f9ea4b
Adriano Ogata <akogata@gmail.com>
2018-12-02 17:53:50 -0200

    Add params for 3rd party API

--- config.js
+++ config.js
@@ -4,14 +4,30 @@ environments.staging = {
   envName: 'staging',
   httpPort: 3000,
   httpsPort: 3001,
-  hashingSecret: 'staging salt'
+  hashingSecret: 'staging salt',
+  payment: { // from stripe
+    apiUserId: process.env.PAYMENT_APIUSERID || '',
+    currency: 'usd'
+  },
+  emailNotification: { // from mailgun
+    apiKey: process.env.EMAILNOTIFICATION_APIKEY || '',
+    domainName: process.env.EMAILNOTIFICATION_DOMAINNAME || ''
+  }
 }
 
 environments.production = {
   envName: 'production',
   httpPort: 5000,
   httpsPort: 5001,
-  hashingSecret: 'production salt'
+  hashingSecret: 'production salt',
+  payment: { // from stripe
+    apiUserId: process.env.PAYMENT_APIUSERID || '',
+    currency: 'usd'
+  },
+  emailNotification: { // from mailgun
+    apiKey: process.env.EMAILNOTIFICATION_APIKEY || '',
+    domainName: process.env.EMAILNOTIFICATION_DOMAINNAME || ''
+  }
 }
 
 const currentEnvironment = typeof process.env.NODE_ENV === 'string' ? process.env.NODE_ENV.toLowerCase() : ''
